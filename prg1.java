40940404bb011e5bb3b5bfa17363598c419c3ee7
GnivethaG <gnivetha2001@gmail.com>
2024-10-21 20:29:21 +0530

    lowercase

---
+++ prg1.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Enter A string");
+        Scanner sc=new Scanner(System.in);
+        String str=sc.next();
+        int i;
+        int count=0;
+        int n=str.length();
+        for(i=0;i<n;i++)
+        {
+
+            char ch=str.charAt(i);
+            int no=(int)ch;
+            if(no>=97 && no<=122)
+            {
+                count++;
+            }
+        }
+        System.out.println("The count of Lowercase is : "+count);
+        sc.close();
+    }
+}
